4ee6e2e5fa1194af318704d48f641f3730852751
Anand Sanmukhani <asanmukh@redhat.com>
2020-09-08 12:32:41 -0400

    start extending the input.go

--- pkg/input/remoteread/remoteread.go
+++ pkg/input/remoteread/remoteread.go
@@ -12,6 +12,7 @@ import (
 	"github.com/prometheus/prometheus/pkg/timestamp"
 	"github.com/prometheus/prometheus/prompb"
 	"github.com/prometheus/prometheus/storage/remote"
+	"github.com/prometheus/prometheus/tsdb/chunkenc"
 	"github.com/thanos-community/obslytics/pkg/input"
 	"github.com/thanos-io/thanos/pkg/extgrpc"
 	"github.com/thanos-io/thanos/pkg/store/storepb"
@@ -29,7 +30,7 @@ type remoteReadInput struct {
 	conf   input.InputConfig
 }
 
-func NewStoreAPIInput(logger log.Logger, conf input.InputConfig) (remoteReadInput, error) {
+func NewRemoteReadInput(logger log.Logger, conf input.InputConfig) (remoteReadInput, error) {
 	return remoteReadInput{logger: logger, conf: conf}, nil
 }
 
@@ -70,7 +71,12 @@ func (i remoteReadInput) Open(ctx context.Context, params input.SeriesParams) (i
 		TLSConfig: tlsConfig,
 	}
 
-	endpointUrl := &config_util.URL{URL: i.conf.Endpoint}
+	parsedUrl, err := url.Parse(i.conf.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	endpointUrl := &config_util.URL{URL: parsedUrl}
 
 	clientConfig := &remote.ClientConfig{
 		URL:              endpointUrl,
@@ -79,43 +85,44 @@ func (i remoteReadInput) Open(ctx context.Context, params input.SeriesParams) (i
 	}
 
 	client, err := remote.NewReadClient("name", clientConfig)
-
 	if err != nil {
 		return nil, err
 	}
 
 	readResponse, err := client.Read(ctx, nil)
-	//readResponse.Get
-	timeseries := readResponse.GetTimeseries()
-	samples := timeseries[0].Samples[0]
-	ser := storepb.Series{
-		Labels: readResponse.Timeseries[0].Labels,
-		Chunks: nil,
-	}
 
-	prompb.Chunk{
-		MinTimeMs:            0,
-		MaxTimeMs:            0,
-		Type:                 0,
-		Data:                 nil,
-		XXX_NoUnkeyedLiteral: struct{}{},
-		XXX_unrecognized:     nil,
-		XXX_sizecache:        0,
-	}
-	storepb.Chunk{
-		Type: 0,
-		Data: nil,
-	}
-	storepb.AggrChunk{
-		MinTime: 0,
-		MaxTime: 0,
-		Raw:     nil,
-		Count:   nil,
-		Sum:     nil,
-		Min:     nil,
-		Max:     nil,
-		Counter: nil,
-	}
+	//readResponse.Get
+	//timeseries := readResponse.GetTimeseries()
+	//timeseries[0].GetSamples()[0].XXX_Marshal()
+	//samples := timeseries[0].Samples[0].
+	//ser := storepb.Series{
+	//	Labels: readResponse.Timeseries[0].Labels,
+	//	Chunks: nil,
+	//}
+
+	//prompb.Chunk{
+	//	MinTimeMs:            0,
+	//	MaxTimeMs:            0,
+	//	Type:                 0,
+	//	Data:                 nil,
+	//	XXX_NoUnkeyedLiteral: struct{}{},
+	//	XXX_unrecognized:     nil,
+	//	XXX_sizecache:        0,
+	//}
+	//storepb.Chunk{
+	//	Type: 0,
+	//	Data: nil,
+	//}
+	//storepb.AggrChunk{
+	//	MinTime: 0,
+	//	MaxTime: 0,
+	//	Raw:     nil,
+	//	Count:   nil,
+	//	Sum:     nil,
+	//	Min:     nil,
+	//	Max:     nil,
+	//	Counter: nil,
+	//}
 	//seriesClient, err := client.Series(ctx, &storepb.SeriesRequest{
 	//	MinTime: timestamp.FromTime(params.MinTime),
 	//	MaxTime: timestamp.FromTime(params.MaxTime),
@@ -128,29 +135,32 @@ func (i remoteReadInput) Open(ctx context.Context, params input.SeriesParams) (i
 	return &readSeriesIterator{
 		logger: i.logger,
 		ctx:    ctx,
-		conn:   nil,
 		client: client}, nil
-}
-
-func ConvertTimeseriesToStoreSeries() {
-
 }
 
 // readSeriesIterator implements input.SeriesIterator
 type readSeriesIterator struct {
-	logger        log.Logger
-	ctx           context.Context
-	conn          *grpc.ClientConn
-	client        remote.ReadClient
-	currentSeries *storepb.Series
+	logger             log.Logger
+	ctx                context.Context
+	client             remote.ReadClient
+	seriesList         []ReadSeries
+	currentSeriesIndex int
+	maxSeriesIndex     int
 }
 
 func (i *readSeriesIterator) Next() bool {
-	return true
+
+	// return false if the last index is already reached
+	if i.currentSeriesIndex+1 > i.maxSeriesIndex {
+		return false
+	} else {
+		i.currentSeriesIndex++
+		return true
+	}
 }
 
 func (i *readSeriesIterator) At() input.Series {
-	return ReadSeries{StoreS: i.currentSeries}
+	return i.seriesList[i.currentSeriesIndex]
 }
 
 func (i *readSeriesIterator) Close() error {
@@ -158,18 +168,90 @@ func (i *readSeriesIterator) Close() error {
 }
 
 // ReadSeries implements input.Series
-type ReadSeries struct{ StoreS *storepb.Series }
+type ReadSeries struct {
+	input.Series
+	timeseries prompb.TimeSeries
+}
 
 func (r ReadSeries) Labels() labels.Labels {
-	panic("implement me")
+
+	var labelList []labels.Label
+	for i := range r.timeseries.Labels {
+		labelList[i] = labels.Label{
+			Name:  r.timeseries.Labels[i].Name,
+			Value: r.timeseries.Labels[i].Value,
+		}
+
+	}
+	return labels.New(labelList...)
 }
 
 func (r ReadSeries) MinTime() time.Time {
-	panic("implement me")
+
+	seconds := r.timeseries.Samples[0].Timestamp / 1000
+	nanoseconds := r.timeseries.Samples[0].Timestamp % 1000 * 1000000
+
+	return time.Unix(seconds, nanoseconds)
 }
 
 func (r ReadSeries) ChunkIterator() (input.ChunkIterator, error) {
-	panic("implement me")
+	//panic("implement me")
+	chunk := ReadChunk{
+		series: r.timeseries,
+	}
+	return chunk.Iterator(), nil
+}
+
+// ReadChunkIterator implements input.ChunkIterator
+type ReadChunkIterator struct {
+	input.ChunkIterator
+	Chunk              ReadChunk
+	currentSampleIndex int
+	maxSampleIndex     int
+}
+
+func (c ReadChunkIterator) Next() bool {
+	// return false if the last index is reached
+	if c.currentSampleIndex+1 > c.maxSampleIndex {
+		return false
+	} else {
+		c.currentSampleIndex++
+		return true
+	}
+}
+
+// Seek advances the iterator forward to the first sample with the timestamp equal or greater than t.
+// If current sample found by previous `Next` or `Seek` operation already has this property, Seek has no effect.
+// Seek returns true, if such sample exists, false otherwise.
+// Iterator is exhausted when the Seek returns false.
+func (c ReadChunkIterator) Seek(t int64) bool {
+	for c.Chunk.series.Samples[c.currentSampleIndex].Timestamp < t {
+		if !c.Next() {
+			return false
+		}
+	}
+	return true
+}
+
+// At returns the current timestamp/value pair.
+// Before the iterator has advanced At behaviour is unspecified.
+func (c ReadChunkIterator) At() (int64, float64) {
+	return c.Chunk.series.Samples[c.currentSampleIndex].Timestamp, c.Chunk.series.Samples[c.currentSampleIndex].Value
+}
+
+// Err returns the current error. It should be used only after iterator is
+// exhausted, that is `Next` or `Seek` returns false.
+func (c ReadChunkIterator) Err() error {
+	return nil
+}
+
+type ReadChunk struct {
+	chunkenc.Chunk
+	series prompb.TimeSeries
+}
+
+func (c ReadChunk) Iterator() ReadChunkIterator {
+	return ReadChunkIterator{Chunk: c, currentSampleIndex: 0, maxSampleIndex: len(c.series.Samples) - 1}
 }
 
 var userAgent = fmt.Sprintf("Obslytics/%s", 0.1)
